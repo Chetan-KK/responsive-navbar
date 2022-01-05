541de652179e45373615f55cc50e132ead1279d1
Chetan Khulage <chetankhulage350@gmail.com>
2022-01-05 16:12:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function openNav() {
+  let navbar = document.getElementById(`navbar`);
+  navbar.style.transform = `translate(0%)`;
+}
+function closeNav() {
+  let navbar = document.getElementById(`navbar`);
+  navbar.style.transform = `translate(100%)`;
+}
